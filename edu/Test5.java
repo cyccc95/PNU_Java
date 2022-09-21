0230fa4a59bf26d9621799fd0874f046d0fb5629
cyccc95 <cyccc95@naver.com>
2022-09-21 17:19:06 +0900

    산술 연산자

---
+++ edu/Test5.java
@@ -0,0 +1,19 @@
+package edu;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		int a = 10 + 2;
+		int b = 10 - 2;
+		int c = 10 * 2;
+		int d = 10 % 2;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
+
+// 산술 연산자
