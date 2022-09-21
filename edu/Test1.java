e79e47dab992438d21a84afed230d65e2d6d72be
cyccc95 <cyccc95@naver.com>
2022-09-21 17:16:32 +0900

    data type

---
+++ edu/Test1.java
@@ -0,0 +1,23 @@
+package edu;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		byte age = 23;
+		short point = 32000;
+		int price = 3500000;
+		long totalSales = 2147483464L;
+		float exChangeRate = 1136.50F;
+		double Amount = 682468.25;
+		
+		System.out.println(age);
+		System.out.println(point);
+		System.out.println(price);
+		System.out.println(totalSales);
+		System.out.println(exChangeRate);
+		System.out.println(Amount);
+	}
+
+}
+
+// 데이터 타입
