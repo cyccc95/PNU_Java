706fb76a1263d48058939d4b36e62190bc44bdb0
cyccc95 <cyccc95@naver.com>
2022-09-21 17:23:56 +0900

    for문, while문, do~while문

---
+++ edu/Test25.java
@@ -0,0 +1,32 @@
+package edu;
+
+public class Test25 {
+
+	public static void main(String[] args) {
+		// for
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("OK");
+	
+		// while
+		int j = 0;
+		while(j<10) {
+			System.out.println(j);
+			j++;
+		}
+		System.out.println("OK");
+		
+		// do~while
+		int k = 10;
+		do {
+			System.out.println(k);
+			k++;
+		} while(k<10);
+		
+	}
+	
+
+}
+
+// for문, while문, do~while문
