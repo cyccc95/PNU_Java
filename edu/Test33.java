25d9fcc6b9ffe8c27c9d1d437071b820f62b6c88
cyccc95 <cyccc95@naver.com>
2022-09-21 17:25:08 +0900

    배열의 선언, 초기화

---
+++ edu/Test33.java
@@ -0,0 +1,20 @@
+package edu;
+
+public class Test33 {
+
+	public static void main(String[] args) {
+		char[] alphabet = {'a','b','c','d'};
+		int[] score = new int[5];
+		score[0] = 90;
+		score[1] = 85;
+		score[2] = 78;
+		score[3] = 100;
+		score[4] = 98;
+		
+		System.out.println(alphabet[0]);
+		System.out.println(score[3]);
+	}
+
+}
+
+// 배열의 선언, 초기화
