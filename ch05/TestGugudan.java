28908e800f8eea6ee521a6c595a33323f9f2cd11
cyccc95 <cyccc95@naver.com>
2022-09-22 13:54:15 +0900

    class Gugudan test

---
+++ ch05/TestGugudan.java
@@ -0,0 +1,13 @@
+package com.ruby.java.ch05;
+
+public class TestGugudan {
+
+	public static void main(String[] args) {
+		Gugudan gg = new Gugudan();
+		
+		gg.printGugudan(2);
+		gg.printGugudan(3);
+		gg.printGugudan(4);
+	}
+
+}
