4f61e1a644109f66d8d46939ab125746c0a5ede9
cyccc95 <cyccc95@naver.com>
2022-09-23 14:29:58 +0900

    자기소개

---
+++ example/Ex02_2.java
@@ -0,0 +1,19 @@
+package example;
+
+public class Ex02_2 {
+
+	public static void main(String[] args) {
+		String name = "최한수";
+		String birth = "1995년 5월 3일";
+		int height = 176;
+		int weight = 65;
+		
+		System.out.println("이름 : " + name);
+		System.out.println("생년월일 : " + birth);
+		System.out.println("키 : " + height + "cm");
+		System.out.println("체중 : " + weight + "kg");
+	}
+
+}
+
+// 자기소개
