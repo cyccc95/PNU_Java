ea779135a0531a71833dfcf7e1740ae9ba418610
cyccc95 <cyccc95@naver.com>
2022-09-25 00:33:00 +0900

    지역별 접속자 수

---
+++ example/Ex04_1.java
@@ -0,0 +1,11 @@
+public class Ex04_1 {
+    public static void main(String[] args) {
+        String[] city = {"서울", "부산", "인천", "대전", "대구"};
+        int[] num = {599, 51, 46, 43, 27};
+        for (int i = 0; i < city.length; i++){
+            System.out.println(city[i] + " : " + num[i] + "명");
+        }
+    }
+}
+
+// 지역별 접속자 수
