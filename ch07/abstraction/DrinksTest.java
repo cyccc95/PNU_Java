0c4112bd864b407e79c70cbe6a4e640da2f487d1
cyccc95 <cyccc95@naver.com>
2022-09-27 14:05:51 +0900

    Drinks class test

---
+++ ch07/abstraction/DrinksTest.java
@@ -0,0 +1,23 @@
+package com.ruby.java.ch07.abstraction;
+
+public class DrinksTest {
+
+	public static void main(String[] args) {
+		Drinks[] drinks = new Drinks[5];
+		drinks[0] = new Juice(1000);	
+		drinks[1] = new Coffee(2000);
+		drinks[2] = new Beer(3000);
+		drinks[3] = new Water(4000);
+		drinks[4] = new Tea(5000);
+		
+		drinks[0].drink();
+		drinks[1].drink();
+		drinks[2].drink();
+		drinks[3].drink();
+		drinks[4].drink();
+		
+
+	
+	}
+
+}
